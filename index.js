7b01763c5b9f0fff5efcfebc66c98b81a81b805e
formosawqd <formosadong@gmail.com>
2025-03-02 22:41:29 +0800

    port 修改

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const userRoutes = require("./routes/userRoutes");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
